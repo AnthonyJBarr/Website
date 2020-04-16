2761a72fb59ef2e0f810f1a7577d26b26f2edf92
AnthonyJBarr <56657352+AnthonyJBarr@users.noreply.github.com>
2020-04-15 20:19:25 -0400

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,39 @@
+/* POPUP CODE */
+function myFunction1() {
+  var popup = document.getElementById("myPopup1");
+  popup.classList.toggle("show");
+}
+
+function myFunction2() {
+  var popup = document.getElementById("myPopup2");
+  popup.classList.toggle("show");
+}
+/* COPY CODE */
+function Copy1() {
+  /* Get the text field */
+  var copyText = document.getElementById("myInput1");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text */
+  alert("Copied the text: " + copyText.value);
+}
+function Copy2() {
+  /* Get the text field */
+  var copyText = document.getElementById("myInput2");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text */
+  alert("Copied the text: " + copyText.value);
+}
